790816f78ec792748d004ab975b563a354b667eb
Evan Vander Hoeven <vhevan.927@gmail.com>
2019-11-19 10:55:08 -0600

    Added refresh button
    added a button to the home view toolbar which will refresh the recycler view with new scan results everytime the button is pressed. (throttling

--- app/src/main/java/com/wifiheatmap/wifiheatmap/HomeFragment.kt
+++ app/src/main/java/com/wifiheatmap/wifiheatmap/HomeFragment.kt
@@ -1,15 +1,19 @@
 package com.wifiheatmap.wifiheatmap
 
+import android.net.wifi.ScanResult
 import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wifiheatmap.wifiheatmap.databinding.FragmentHomeBinding
+import com.wifiheatmap.wifiheatmap.room.Network
+import timber.log.Timber
 
 /**
  * The home fragment for the Wifi heat mapping application
@@ -20,6 +24,9 @@ import com.wifiheatmap.wifiheatmap.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
+    private lateinit var recyclerAdapter: HomeViewAdapter
+    private var networks : List<Network>? = null
+    private var scanResults : List<ScanResult>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +37,7 @@ class HomeFragment : Fragment() {
 
         // setup the recyclerView
         val recyclerLayout = LinearLayoutManager(this.context)
-        val recyclerAdapter = HomeViewAdapter()
+        recyclerAdapter = HomeViewAdapter()
 
         binding.networksRecyclerView.apply{
             setHasFixedSize(true)
@@ -38,25 +45,49 @@ class HomeFragment : Fragment() {
             adapter = recyclerAdapter
         }
 
-        // link the ViewModel and the SandwichAdapter
-//        val viewModel = ViewModelProviders.of(this)
-//            .get(outViewModelClass::class.java)
+        // link the ViewModel and the HomeViewAdapter
+        val viewModel = ViewModelProviders.of(this).get(ViewModel::class.java)
 
-//        // listen to the viewModel's data and attach it to the adapter
-//        viewModel.getDataMethod().observe(this, object: Observer<List<networkObject>> {
-//            // store a reference to the adapter in this anonymous class
-//            private val adapter = recyclerAdapter
-//
-//            override fun onChanged(t: List<networkObject>?) {
-//                if (t != null) {
-//                    adapter.setNetworks(t, List of visible networks)
-//                }
-//            }
-//        }
+        // listen to the viewModel's data and attach it to the adapter
+        viewModel.getNetworks().observeForever{
+            networks = it
+            if (scanResults != null) {
+                // I don't have to worry about networks being null,
+                // because I set it above explicitly.
+                recyclerAdapter.setNetworks(networks!!, scanResults!!)
+            }
+        }
 
+        // Make a first call to the updateScanResults so that the view
+        // will pull that in right away and not wait for the user to
+        // press a button.
+        this.updateScanResults()
 
+        setHasOptionsMenu(true)
         return this.binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        val menuItem = menu.add(Menu.NONE, R.id.refresh_network_list, Menu.NONE,
+            R.string.refresh)
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (item.itemId == R.id.refresh_network_list) {
+            this.updateScanResults()
+            true
+        } else {
+            Timber.e("Unrecognized options item: %s", item.itemId)
+            false
+        }
+    }
+
+    private fun updateScanResults()
+    {
+        // TODO use asynchronous function
+        //  MainActivity when that's ready.
+        Toast.makeText(this.context, "ScanResults not implemented", Toast.LENGTH_SHORT).show()
+    }
 }
