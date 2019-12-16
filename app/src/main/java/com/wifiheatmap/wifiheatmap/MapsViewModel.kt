0a0d12eabae2b86eb58b329953e0af464ebc609a
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2019-12-15 21:16:53 -0600

    Fix filter by date range

--- app/src/main/java/com/wifiheatmap/wifiheatmap/MapsViewModel.kt
+++ app/src/main/java/com/wifiheatmap/wifiheatmap/MapsViewModel.kt
@@ -3,13 +3,14 @@ package com.wifiheatmap.wifiheatmap
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import java.util.*
 
 class MapsViewModel(application: Application) : AndroidViewModel(application) {
 
     val isDarkModeEnabled = MutableLiveData<Boolean>(false)
     val isColorBlindModeEnabled = MutableLiveData<Boolean>(false)
-    val startDate = MutableLiveData<String>("")
-    val endDate = MutableLiveData<String>("")
+    val startDate = MutableLiveData<Date>()
+    val endDate = MutableLiveData<Date>()
     val refreshRate = MutableLiveData<Double>(5.0)
     val viewNetwork = MutableLiveData<String>("")
     val tileSize = MutableLiveData<Double>(0.5)
